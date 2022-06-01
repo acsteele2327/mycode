cf0c3d3f36d9a1d59a09a5229cc1b9eb3c358df7
Aaron <acsteele2327@users.noreply.github.com>
2022-06-01 20:51:18 +0000

    Learning about the shebang and input functions

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#dis my test code
+
+def main():
+
+    user_input = input("Please enter an IPv4 IP address: ")
+
+
+    print("You told me the IPv4 address is:", user_input)
+
+    #adding this line to ask user for vendor name
+
+    vendor = input("Please input the name of the vendor: ")
+
+    print("You told me the vendor name was: ", vendor)
+
+main()
